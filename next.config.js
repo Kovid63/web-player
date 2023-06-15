6b64e350507f104694317eb4f1f776da0336c8ea
Kovid63 <kovid63@gmail.com>
2023-06-16 04:29:10 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['i.scdn.co','encrypted-tbn0.gstatic.com', 'img.icons8.com'],
+    domains: ['i.scdn.co','encrypted-tbn0.gstatic.com', 'img.icons8.com', 'drive.google.com'],
   },
 }
 
